2e58a87bb622651dbc59cfd12c427f5acf7a18eb
Sriram Mishra <srirammishra39@gmail.com>
2024-04-06 22:25:58 +0530

    Hello World

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is useless website full of useless resources.</h1>
+      <p id='quote'>"The production of too many useful things results in too many useless people." - Karl Marx</p>
+    </div>
+  );
+}
+
+export default App;
