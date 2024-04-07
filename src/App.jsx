ed909d7c3eaff7c2b2b01c695c19e6cb90003dac
Sriram Mishra <130164844+SerratedC@users.noreply.github.com>
2024-04-07 11:29:44 +0530

    Fix grammatical errors: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div style={{textAlign: 'center'}}>
-      <h1>This is useless website full of useless resources ;)</h1>
+      <h1>This is a useless website full of useless resources ;)</h1>
       <p>(Work in progress)</p>
       <p id='quote'><i>"The production of too many useful things results in too many useless people." - Karl Marx</i></p>
       </div>
