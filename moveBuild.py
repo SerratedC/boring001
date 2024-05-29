49fde2ba158f934589d96b41bc7dd20979478a27
Sriram Mishra <srirammishra39@gmail.com>
2024-05-29 18:21:46 +0530

    Final Release: Project Depriciated Because it's Useless

---
+++ moveBuild.py
@@ -0,0 +1,9 @@
+import shutil
+shutil.move("build/asset-manifest.json", ".")
+shutil.move("build/favicon.png", ".")
+shutil.move("build/index.html", ".")
+shutil.move("build/manifest.json", ".")
+shutil.move("build/precache-manifest.99ac1f98e3ecda5c658c86e62d008363.js", ".")
+shutil.move("build/robots.txt", ".")
+shutil.move("build/service-worker.js", ".")
+shutil.move("build/static", ".")
---
+++ precache-manifest.99ac1f98e3ecda5c658c86e62d008363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35350400caf32cb4415b8fd98bf133c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "430c32a37ca5a2a0b1aa",
+    "url": "/static/css/main.3bcc4b52.chunk.css"
+  },
+  {
+    "revision": "fda8dd5c61822d5fbf6a",
+    "url": "/static/js/2.98bc67df.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "430c32a37ca5a2a0b1aa",
+    "url": "/static/js/main.c6321b12.chunk.js"
+  },
+  {
+    "revision": "8a5626378c6138c21833",
+    "url": "/static/js/runtime~main.cef9ab07.js"
+  }
+]);
